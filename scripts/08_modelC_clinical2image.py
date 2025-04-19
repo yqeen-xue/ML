6d6f24af0325aedc7c1680ce30f43430f71450d2
yaxuan <kq24393@bristol.ac.uk>
2025-04-19 02:31:55 +0100

    08 Standardize patient IDs by removing suffixes

--- scripts/08_modelC_clinical2image.py
+++ scripts/08_modelC_clinical2image.py
@@ -27,7 +27,7 @@ def load_image_features(image_feat_path):
     image_feat = pd.read_csv(image_feat_path)
     image_feat.reset_index(inplace=True)
     image_feat.rename(columns={"index": "PatientID"}, inplace=True)
-    image_feat["PatientID"] = image_feat["PatientID"].str.extract(r"(R01-\d+|AMC-\d+)")
+    image_feat["PatientID"] = image_feat["PatientID"].astype(str).str.extract(r"(R01-\d+|AMC-\d+)")
 
     return image_feat
 
